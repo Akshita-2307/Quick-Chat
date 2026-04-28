8a9b2af025ccff2d43bc157c25327b5769adaf95
Akshita <akpandey2307@gmail.com>
2026-04-29 00:04:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const loginbtn = document.getElementById("loginbtn");
 loginbtn.addEventListener("click", function () {
     console.log("clicked");
-    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(username,password);
+    if (username == "" || password == "") {
+        alert("Username and Password can't be empty!");
+        return;
+    }
 });
+let user=JSON.parse(localStorage.getItem("user")) || [];
+let existinguser= user.find(function(user){
+    return user.username===username;
+});
+if(existinguser){
+    if(existinguser.password===password){
+        localStorage.setItem("currentuser",username);
+        window.location.href="index.html";
+    }else{
+        alert("Incorrect Password");
+    }
+} else{
+    user.push({
+        username:username,
+        password:password
+    });
+    localStorage.setItem("users",JSON.stringify(users));
+    localStorage.setItem("currentuser",username);
+    window.location.href="chat.html";
+}
